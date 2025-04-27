9a924af17f329741a61a39b338a2cb36e9ced8e3
Mihalache Ionut Marius <98221678+MariusMihalache@users.noreply.github.com>
2025-04-27 22:11:35 +0300

    Update auto_repair.sh

--- bash/auto_repair.sh
+++ bash/auto_repair.sh
@@ -1,1 +1,14 @@
+#!/bin/bash
+
+./check_gateway.sh
+if [ $? -ne 0 ]; then
+  echo "Repairing gateway..."
+  systemctl restart network.service
+fi
+
+./check_dns.sh
+if [ $? -ne 0 ]; then
+  echo "Repairing DNS..."
+  systemctl restart systemd-resolved
+fi
 
